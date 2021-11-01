3e70b603eca3994553253e2549f7c26de4f48ea6
mbanya <mbanyaw@gmail.com>
2021-11-01 12:38:11 +0300

    DDD and a couple of packages

--- src/Domains/Customer/Models/User.php
+++ src/Domains/Customer/Models/User.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace Domains\Customer;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Domains\Customer\Models\Concerns\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUuid;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'uuid',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -33,12 +35,5 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+
 }
